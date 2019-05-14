a8eb813dd8978f082ad2ba59020fac123fbb725e
Philip Davis <philip.e.davis@rutgers.edu>
2019-05-14 12:50:57 -0400

    Added sample counter and updated run script to be selectable

--- run.vm.sh
+++ run.vm.sh
@@ -1,21 +1,20 @@
 #/bin/bash -x
 
-rm -f ebstrace* driver_trace/*
-
 atmode=trace
 
 TAU2DIR=`pwd`/tau2
 export PATH=$PATH:${TAU2DIR}/x86_64/bin
 
-if [ "$atmode" == "trace" ] ; then
+if [ "$atmode" == "profile" ] ; then
     # Profiling
     export TAU_CALLPATH=1    # turns on call path profiling
     export TAU_CALLPATH_DEPTH=100   # should be deep enough
     export TAU_EBS_KEEP_UNRESOLVED_ADDR=1
     export TAU_PROFILE_PREFIX=driver
     export TAU_PROFILE_FORMAT=merged
-elif [ "$atmode" == "profile" ] ; then
+elif [ "$atmode" == "trace" ] ; then
     #Tracing
+    rm -f ebstrace* driver_trace/*
     export TAU_TRACE=1
     export TRACEDIR=`pwd`/driver_trace
 fi
--- taustubs-driver.cpp
+++ taustubs-driver.cpp
@@ -1,5 +1,6 @@
 #include "adios2/toolkit/profiling/taustubs/tautimer.hpp"
 #include <iostream>
+#include <cstdlib>
 
 #include<mpi.h>
 
@@ -7,19 +8,50 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    int root = 0;
+    int rank, size;
     MPI_Init(&argc, &argv);
 
-    cout<<"Starting Timer..."<<endl;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);    
 
-    TAU_START("a");
+    if(!rank) {
+        cout << "Starting Timer..." <<endl;
+        TAU_METADATA("appname", "ADIOS2");
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    for(int i = 0; i < 1024*1024; i++);
+    TAU_METADATA("myRank", to_string(rank).c_str());
 
-    TAU_STOP("a");
+    TAU_START("timer_name");
 
-    cout<<"Ended timer."<<endl;
+    for(int i = 0; i < 20; i++) {
+        stringstream tsname;
+        int new_root;        
+
+        //busy wait a while so we can see a gap in the trace
+        for(int j = 0; j < 1024; j++) {
+            TAU_SAMPLE_COUNTER("rand_sample", 900 + (rand() % 201));
+        }
+
+        tsname << "ts" << i;
+        TAU_SCOPED_TIMER(tsname.str().c_str());
+        
+
+        new_root = rand() % size;
+        MPI_Bcast(&new_root, 1, MPI_INT, root, MPI_COMM_WORLD);
+        root = new_root;
+    }
+
+    TAU_STOP("timer_name");
+
+    for(int j = 0; j < 1024; j++) {
+        TAU_SAMPLE_COUNTER("rand_sample", 900 + (rand() % 201));
+    }
+
 
     MPI_Barrier(MPI_COMM_WORLD);
+    
 
     MPI_Finalize();
 
