fdb9bb3c989accf6714a64470b2d1e7a182357f1
Philip Davis <philip.e.davis@rutgers.edu>
2019-05-14 11:14:23 -0400

    Added vm script

---
+++ run.vm.sh
@@ -0,0 +1,30 @@
+#/bin/bash -x
+
+rm -f ebstrace* driver_trace/*
+
+atmode=trace
+
+TAU2DIR=`pwd`/tau2
+export PATH=$PATH:${TAU2DIR}/x86_64/bin
+
+if [ "$atmode" == "trace" ] ; then
+    # Profiling
+    export TAU_CALLPATH=1    # turns on call path profiling
+    export TAU_CALLPATH_DEPTH=100   # should be deep enough
+    export TAU_EBS_KEEP_UNRESOLVED_ADDR=1
+    export TAU_PROFILE_PREFIX=driver
+    export TAU_PROFILE_FORMAT=merged
+elif [ "$atmode" == "profile" ] ; then
+    #Tracing
+    export TAU_TRACE=1
+    export TRACEDIR=`pwd`/driver_trace
+fi
+
+mpirun -n 8 tau_exec -T mpi,pthread -ebs ./taustubs > driver.log 2>&1
+
+if [ "$atmode" == "trace" ] ; then
+    cd driver_trace
+    tau_treemerge.pl
+
+    tau2slog2 tau.trc tau.edf -o tau.slog2
+fi
